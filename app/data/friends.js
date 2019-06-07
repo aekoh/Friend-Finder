fc21ab80bfef9cbd83874651174c29577a199553
aine <aineekohh@yahoo.com>
2019-06-07 13:33:30 -0500

    i added a form with values to my survey html file

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ var friends = [
     {
         name: "Stark",
         photo: "https://i.imgur.com/fMo86mo.jpg",
-        scores: [1,5,3,5,5,3,4,2,5,2]
+        scores: [1,5,5,3,4,1,5,1,5,1]
     },
     {
         name: "America",
         photo: "https://i.imgur.com/pwP69QZ.jpg",
-        scores: [1,3,4,4,2,2,3,3,3,5]
+        scores: [5,5,1,1,1,5,4,5,1,1]
     },
     {
         name: "Thor",
         photo: "https://i.imgur.com/5TfODZo.jpg",
-        scores: [1,4,5,3,2,5,4,4,3,2]
+        scores: [1,4,5,5,5,4,5,2,3,5]
     },
     {
         name: "Hulk-yonce'",
         photo: "https://i.imgur.com/5TSgMkc.jpg",
-        scores: [1,5,5,4,2,4,3,4,3,3]
+        scores: [3,1,1,3,5,3,3,4,3,5]
     }
 
 ];
